c237cf03aa1187f5ba01373923a237bf88a2a7eb
Jayant Malik <dev.jayantmalik@gmail.com>
2020-07-26 22:30:59 +0530

    added search page and search components to router

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,5 +1,20 @@
 import React from "react";
+import MoviesList from "../../components/MoviesList";
+import fakeData from "../../api";
+import Paginator from "../../components/Paginator";
+import Alert from "../../components/Alert";
 
-const Search = () => null;
+const Search = () => {
+  return (
+    <main className="container-fluid pt-3">
+      <section>
+        <h2 className="heading">Search Results</h2>
+        <Alert error message="No results found" />
+        {/* <MoviesList movies={fakeData} />
+        <Paginator /> */}
+      </section>
+    </main>
+  );
+};
 
 export default Search;
