0a7c15df11e0c0242bc0beda24dd083561d684bd
Jayant Malik <dev.jayantmalik@gmail.com>
2020-07-26 17:11:42 +0530

    added category page

--- src/pages/Category/index.js
+++ src/pages/Category/index.js
@@ -1,5 +1,18 @@
 import React from "react";
+import MoviesList from "../../components/MoviesList";
+import fakeData from "../../api";
+import Paginator from "../../components/Paginator";
 
-const Category = () => null;
+const Category = () => {
+  return (
+    <main className="container-fluid pt-3">
+      <section>
+        <h2 className="heading">Action Movies</h2>
+        <MoviesList movies={fakeData} />
+        <Paginator />
+      </section>
+    </main>
+  );
+};
 
 export default Category;
