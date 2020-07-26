7040adf32d463b0785ea71dccf58c3e7f56598f8
Jayant Malik <dev.jayantmalik@gmail.com>
2020-07-26 16:54:44 +0530

    fixed router paths and added css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Root from "./Root";
+import "./assets/css/bootstrap.min.css";
+import "./assets/css/custom.css";
 
 ReactDOM.render(<Root />, document.getElementById("root"));
 
