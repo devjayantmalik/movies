8d985f30a79cbcbf870ae5e958d68aba6151ceec
devjayantmalik <devjayantmalik@users.noreply.github.com>
2020-07-27 14:05:05 +0000

    deploy: eb1968f20e7d1e7c67d032d545ad5fb4d6ebec30

---
+++ precache-manifest.3751e3bf33c1aaa3c0fbbdd599aa5a37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67e84b12f2d56da23e2d9902e815a0fd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "86a7d2610329ee868b8b",
+    "url": "./static/css/main.6e704edc.chunk.css"
+  },
+  {
+    "revision": "7b11154a9f0723bac19d",
+    "url": "./static/js/2.b14ee8dd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.b14ee8dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86a7d2610329ee868b8b",
+    "url": "./static/js/main.49be4aaa.chunk.js"
+  },
+  {
+    "revision": "a70115eb644411f117cf",
+    "url": "./static/js/runtime-main.bd7c1f81.js"
+  }
+]);
