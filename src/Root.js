d227794243c71b9706bce82602db46947f2a32e3
Jayant Malik <dev.jayantmalik@gmail.com>
2020-07-31 22:32:59 +0530

    added react-redux provider component to root

--- src/Root.js
+++ src/Root.js
@@ -1,6 +1,12 @@
 import React from "react";
 import Router from "./routers";
+import { Provider } from "react-redux";
+import createStore from "./services/store";
 
-const Root = () => <Router />;
+const Root = () => (
+  <Provider store={createStore()}>
+    <Router />
+  </Provider>
+);
 
 export default Root;
