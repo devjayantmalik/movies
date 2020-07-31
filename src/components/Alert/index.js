60d86fa7085be596d0dbe55fd1d3500ee3b39ba7
Jayant Malik <dev.jayantmalik@gmail.com>
2020-07-31 22:28:47 +0530

    added info style to alert

--- src/components/Alert/index.js
+++ src/components/Alert/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Alert = ({ message, error, success }) => {
+const Alert = ({ message, error, success, info }) => {
   const classes = ["alert", "my-4", "lead", "font-weight-bold"];
 
   if (!!error) classes.push("alert-danger");
   if (!!success) classes.push("alert-success");
+  if (!!info) classes.push("alert-info");
 
   return (
     <div className="container text-center">
@@ -17,6 +18,7 @@ const Alert = ({ message, error, success }) => {
 Alert.propTypes = {
   success: PropTypes.bool,
   error: PropTypes.bool,
+  info: PropTypes.bool,
   message: PropTypes.string.isRequired,
 };
 
