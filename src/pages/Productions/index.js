a39a793516226ef6b20b9881f68bfaef121971d0
Jayant Malik <dev.jayantmalik@gmail.com>
2020-07-31 22:32:12 +0530

    refactored components to class based components

--- src/pages/Productions/index.js
+++ src/pages/Productions/index.js
@@ -3,16 +3,22 @@ import ProductionList from "../../components/ProductionList";
 import fakeData from "../../api";
 import Paginator from "../../components/Paginator";
 
-const Productions = () => {
-  return (
-    <main className="container-fluid pt-3">
-      <section>
-        <h2 className="heading">Productions</h2>
-        <ProductionList productions={fakeData} />
-        <Paginator />
-      </section>
-    </main>
-  );
-};
+class Productions extends React.Component {
+  state = { loading: true };
+
+  componentDidMount() {}
+
+  render() {
+    return (
+      <main className="container-fluid pt-3">
+        <section>
+          <h2 className="heading">Production Videos</h2>
+          <ProductionList productions={fakeData} />
+          <Paginator />
+        </section>
+      </main>
+    );
+  }
+}
 
 export default Productions;
--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,20 +1,54 @@
 import React from "react";
-// import MoviesList from "../../components/MoviesList";
-// import fakeData from "../../api";
-// import Paginator from "../../components/Paginator";
+import MoviesList from "../../components/MoviesList";
+import fakeData from "../../api";
+import Paginator from "../../components/Paginator";
 import Alert from "../../components/Alert";
+import { connect } from "react-redux";
 
-const Search = () => {
-  return (
-    <main className="container-fluid pt-3">
-      <section>
-        <h2 className="heading">Search Results</h2>
-        <Alert error message="No results found" />
-        {/* <MoviesList movies={fakeData} />
-        <Paginator /> */}
-      </section>
-    </main>
-  );
+class Search extends React.Component {
+  state = { loading: false };
+  componentDidMount() {
+    this.setState({ loading: true });
+
+    setTimeout(() => {
+      this.setState({ loading: false });
+    }, 2000);
+  }
+
+  renderLoader = () => {
+    return this.state.loading ? (
+      <Alert info message="Fetching your data..." />
+    ) : null;
+  };
+
+  renderResults = () => {
+    if (!this.state.loading) {
+      return (
+        <>
+          <MoviesList movies={this.props.results} />
+          <Paginator />
+        </>
+      );
+    }
+
+    return null;
+  };
+
+  render() {
+    return (
+      <main className="container-fluid pt-3">
+        <section>
+          <h2 className="heading">Search Results</h2>
+          {this.renderLoader()}
+          {this.renderResults()}
+        </section>
+      </main>
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return { results: fakeData };
 };
 
-export default Search;
+export default connect(mapStateToProps, {})(Search);
